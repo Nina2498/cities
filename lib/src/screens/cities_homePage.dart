7e2037cde8a28afaf3a3230d2dfd578ebee46b06
Nina2498 <shene1998m@gmail.com>
2022-08-05 18:26:18 +0300

    done

--- lib/src/screens/cities_homePage.dart
+++ lib/src/screens/cities_homePage.dart
@@ -30,7 +30,7 @@ class CitiesHomePage extends StatelessWidget {
             children: [
               Container(
                 color: Color.fromARGB(255, 51, 88, 151),
-                height: 710,
+                height: 722,
                 child: ListView.builder(
                   itemCount: listofCities.length,
                   itemBuilder: (context, index){
@@ -52,27 +52,33 @@ class CitiesHomePage extends StatelessWidget {
                                                 )
                      );
                    },
-                            child: GridTile(
-                             child: Image.network(listofCities[index]
-                             ["image"].toString()),
+                            child: Stack(
+                             children: <Widget>[
+                             Container(
+                               child: Image.network(listofCities[index]
+                               ["image"].toString()),
+                               height: 250,
+                               width: double.infinity,
+                              
+                             ),
 
-                            footer: Container(
-                                          padding: EdgeInsets.all(8),
-                                          color: Colors.blue.withOpacity(.5),
-                                          child: Text(
-                                          listofCities[index]["id"].toString(),
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.white,
-                                              fontSize: 24,
-                                            ),
-                                          ),
-                                        ),
-                             
-                    ), 
+                           
                     
-                     ) 
-                     );
+                    Container(
+                     margin: EdgeInsets.only(top: 200),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(color: Color.fromARGB(255, 114, 118, 123),
+                     borderRadius: BorderRadius.circular(35)),
+                
+                  child: Text(listofCities[index]["id"].toString(), 
+                              style: TextStyle(
+                               color: Colors.white,
+                               fontWeight: FontWeight.bold, 
+                               fontSize: 24),),),
+            ]
+                     
+                     ) )); 
+                      
                   }
                 )
            ),
@@ -80,92 +86,8 @@ class CitiesHomePage extends StatelessWidget {
            
  
             ]
-        )));
+    )));
             
           }
         }
 
-
-
-
-
-
-
-// Container(
-//         width: 300,
-//         height: 300,
-//         color: Colors.transparent,
-//         child: ClipRRect(
-//           borderRadius: BorderRadius.circular(12),
-//           child: InkWell(
-//             onTap: () {},
-//             child: GridTile(
-//               child: Image.network(listofCities[index]
-//                               ["image"].toString()),
-//                 fit: BoxFit.cover,
-//               ),
-              // footer: Container(
-              //   padding: EdgeInsets.all(8),
-              //   color: Colors.blue.withOpacity(.5),
-              //   child: Text(
-              //    listofCities[index]["id"].toString(),
-              //     style: TextStyle(
-              //       fontWeight: FontWeight.bold,
-              //       color: Colors.white,
-              //       fontSize: 24,
-              //     ),
-              //   ),
-              // ),
-//             ),
-//           ),
-//         ),
-//       )
-
-
-
-
-  // child: Container(
-  //               color: Color.fromARGB(255, 51, 88, 151),
-  //               height: 500,
-  //               child: ListView.builder(
-  //                 itemCount: listofCities.length,
-  //                 itemBuilder: (context, index){
-                  
-  //                   return Container(
-  //                     padding: EdgeInsets.all(10),
-
-  //                     child: Column(
-  //                       children: [
-  //                       Container(
-  //                           child:Image.network(listofCities[index]
-  //                             ["image"].toString()),
-                          
-                              
-  //                         ),
-  //                        Positioned(
-  //                           child: InkWell(
-  //                            onTap: () {
-                               
-                                  
-                                        
-
-  //                       child: Container(alignment: Alignment.center,
-  //                        decoration: BoxDecoration(color: Color.fromARGB(255, 114, 118, 123),
-  //                       borderRadius: BorderRadius.circular(35)),
-  //                             child: Text(listofCities[index]["id"].toString(), 
-  //                             style: TextStyle(
-  //                              color: Colors.white,
-  //                              fontWeight: FontWeight.bold, 
-  //                              fontSize: 24),),),
-  //                           )
-  //                         )
-  //                       ],
-  //                     ),
-  //                   );
-  //                 }
-  //                 )
-  //                 )
-  //                 )
-  //               ]),
-  //             ),
-  //           );
